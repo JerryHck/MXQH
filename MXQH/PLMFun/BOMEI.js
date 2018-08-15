14ed6f325e1b852d805f3e4de00c89af8ab357d0
liufei <491675469@qq.com>
2018-08-15 12:11:04 +0800

    PLM电子料库导入导出BOM功能

---
+++ MXQH/PLMFun/BOMEI.js
@@ -0,0 +1,43 @@
+﻿'use strict';
+
+angular.module('app')
+.controller('BOMEICtrl', ['$scope', '$http', '$q', 'AjaxService','$window',
+function ($scope, $http, $q, AjaxService,$window) {
+
+    var bomei = this;
+    bomei.Import = ImportExcel;
+    bomei.Export = ExportExcel;
+    bomei.Do = Do;
+    bomei.FileData = { header: { header: ["Line","Code","SPEC","Name" ] }, sheetNum: 1, data: [] };
+    
+    //导入excel数据
+    function ImportExcel() {
+        console.log('1');
+        var en = {};
+        en.ListLoad = JSON.stringify(bomei.List);
+        en.TempColumns = 'ListLoad';
+        bomei.promise = AjaxService.ExecPlan("AuctusBom", "AuctusImport", en).then(function (data) {
+            //更新功能基本信息
+            toastr.success('储存成功');
+        })
+    }
+    //导出excel数据
+    function ExportExcel() {
+        var en = {};
+        var sheet = {};
+        sheet.SheetName = "BOM列表";
+        sheet.ColumnsName = ["层次号","组号", "料品", "品名", "规格"]
+        sheet.FirstColunms = false;
+        //console.log(itemmaster.S);
+        bomei.promise = AjaxService.ExecPlanToExcel("AuctusBom", 'AuctusBOM', en, sheet).then(function (data) {
+            $window.location.href = data.File;
+        });
+    }
+    function Do() {
+        bomei.List = angular.copy(bomei.FileData.data[0]);
+        bomei.List.splice(0,1);
+        //console.log(bomei.FileData.data[0]);
+    }
+}
+
+]);
