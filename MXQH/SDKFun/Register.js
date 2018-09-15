273e0a1a5e9d5803d103bd42c54b7180c3fdd338
liufei <491675469@qq.com>
2018-09-15 09:01:46 +0800

    SDk注册页添加，未开发完成

---
+++ MXQH/SDKFun/Register.js
@@ -0,0 +1,26 @@
+﻿'use strict';
+
+angular.module('RegisterApp', [
+    'appData',
+    'ngAnimate',
+    'toastr',
+    'ngCookies',
+    'ngSanitize',
+    'AjaxServiceModule',
+    'ngMessages',
+      'ui.bootstrap'
+])
+var app = angular.module('RegisterApp').controller('RegisterCtrl', RegisterCtrl);
+RegisterCtrl.$inject = ['$scope', 'AjaxService', 'toastr', 'MyPop', 'appUrl', '$cookieStore', '$window'];
+function RegisterCtrl($scope, AjaxService, toastr, MyPop, appUrl, $cookieStore, $window) {
+    var vm = this;
+    vm.Register = Register;
+    vm.SelectTab = SelectTab;
+    //注册用户
+    function Register() {
+        console.log('123');
+    }
+    function SelectTab(index) {
+        vm.Focus = index;
+    }
+}
