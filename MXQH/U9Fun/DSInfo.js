9e1dcc68751d5b999a9c5740a1b730409de4c744
liufei <491675469@qq.com>
2018-10-09 16:00:49 +0800

    DSInfo修改

--- MXQH/U9Fun/DSInfo.js
+++ MXQH/U9Fun/DSInfo.js
@@ -1,25 +1,48 @@
 ﻿'use strict';
 
 angular.module('app')
-.controller('DSInfoCtrl', ['$scope', '$http', 'Dialog', 'toastr', 'AjaxService', 'MyPop',
-function ($scope, $http, Dialog, toastr, AjaxService, Form, MyPop) {
+.controller('DSInfoCtrl', ['$rootScope', '$scope', '$http', 'Dialog', 'toastr', 'AjaxService', 'MyPop',
+function ($rootScope,$scope, $http, Dialog, toastr, AjaxService, Form, MyPop) {
 
     var dsinfo = this;
     dsinfo.page = { index: 1, size: 15, maxSize: 10 };
     dsinfo.S = {};
     dsinfo.DataBind = DataBind;
     dsinfo.Search = Search;
+    dsinfo.BatchSave = BatchSave;
     dsinfo.Edit = Edit;
+    dsinfo.List = {};
     DataBind();
     
+    function BatchSave() {
+        var li = [];
+        angular.forEach(dsinfo.List, function (r, i) {
+            var en = {};
+            en.ID = r.ID;
+            en.ModifyBy = $rootScope.User.UserNo
+            en.Remark = r.Remark;
+            li.push(en);
+        });
+        console.log(li);
+        var json = {};
+        json.List = JSON.stringify(li);
+        json.TempColumns = "List"
+        console.log(json);
+        dsinfo.promise = AjaxService.ExecPlan("MRPDSInfoResult", "BatchUpdate", json).then(function (data) {
+            toastr.success('储存成功');
+            //更新功能基本信息
+            //AjaxService.LoginAction("ReflashRoot");
+        })
+    }
+
     //绑定数据（带分页）
     function DataBind() {
         var list2 = [];
         if (dsinfo.S.Code) {
-            list2.push({ name: "Code", value: dsinfo.S.Code });
+            list2.push({ name: "Code", value: '%'+dsinfo.S.Code+'%' });
         }
-        if (dsinfo.S.DocNo) {
-            list2.push({ name: "DocNo", value: dsinfo.S.DocNo });
+        if (dsinfo.S.PlanCode) {
+            list2.push({ name: "PlanCode", value:'%'+dsinfo.S.PlanCode+'%' });
         }
         dsinfo.promise = AjaxService.GetPlansPage("MRPDSInfoResult", list2, dsinfo.page.index, dsinfo.page.size).then(function (data) {
             dsinfo.List = data.List;
