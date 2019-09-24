b034511e4d7df23dc8f9b84f808f73035868b1a0
6009000134 <491675469@qq.com>
2019-09-24 15:52:04 +0800

    U9 1、研发风险备料报表 2、供应商8周交货计划

---
+++ MXQH/UIFun/U9Fun/Report/RiskPreparationRep.js
@@ -0,0 +1,45 @@
+﻿'use strict';
+angular.module('app')
+.controller('RiskPreCtrl', ['$rootScope', '$scope', 'Dialog', 'toastr', 'AjaxService', 'MyPop', '$window',
+function ($rootScope, $scope, Dialog, toastr, AjaxService, MyPop, $window) {
+    var vm = this;
+    vm.page = { pageIndex: 1, pageSize:10, maxSize: 10 };
+    vm.Search = Search;
+    vm.DataBind = DataBind;
+    vm.Export = Export;
+    Init();
+    //DataBind();
+    //初始化
+    function Init() {
+        vm.Ser = {};
+    }
+    //数据绑定
+    function DataBind() {
+        vm.promise = AjaxService.ExecPlan("AuctusForecast", "RiskList", vm.page).then(function (data) {
+            //console.log(data);
+            vm.List = data.data;
+            vm.page.total = data.data1[0].Count;
+        });
+    }
+    //查询
+    function Search(validForm) {
+        //表单验证是否通过
+        if (validForm.$invalid) {
+            toastr.error('订单号不能为空！');
+            return;
+        }
+        vm.page.pageIndex = 1;
+        DataBind();
+    }
+
+   
+    //导出
+    function Export() {
+        vm.page.pageSize = 1000000;
+        vm.promise = AjaxService.GetPlanExcel("AuctusForecast", "RiskList", vm.page).then(function (data) {
+            $window.location.href = data.File;
+        });
+        vm.page.pageSize = 10;
+    }
+}
+])
