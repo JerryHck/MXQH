0b56d5b4351cc52c3e6ebde53649f9340c1880cb
6009000134 <491675469@qq.com>
2020-01-02 16:12:21 +0800

    MES 完工报告填写功能

---
+++ MXQH/UIFun/MESFun/MO/CompleteRptDialog.js
@@ -0,0 +1,111 @@
+﻿'use strict';
+angular.module('app')
+.controller('CompleteRptDialogCtrl', ['$rootScope', '$scope', 'ItemData', '$uibModalInstance', 'Dialog', 'toastr', 'AjaxService', 'Form',
+function ($rootScope, $scope, ItemData, $uibModalInstance, Dialog, toastr, AjaxService, Form) {
+    var vm = this;
+    vm.Item = {};
+    vm.Save = Save;
+    vm.Cancel = Cancel;
+    vm.SelectProduct = SelectProduct;//选择料号
+    vm.SelectWorkOrder = SelectWorkOrder;
+    Init();
+    //初始化
+    function Init() {
+        if (ItemData.ID) {//编辑
+            vm.Item = ItemData;
+        } else {//新增
+            vm.Item.CompleteDate = GetCurrentDate();
+        }
+    }
+
+    //保存
+    function Save() {
+        var en = {};
+        var li = [];
+        li.push(vm.Item);
+        en.EntityInfo = JSON.stringify(li);
+        en.TempColumns = "EntityInfo";
+        if (vm.Item.ID) {//编辑操作
+            vm.promise = AjaxService.ExecPlan("CompleteRpt", "Update", en).then(function (data) {
+                if (data.data[0].MsgType == "0") {
+                    toastr.error(data.data[0].Msg);
+                } else {
+                    toastr.success(data.data[0].Msg);
+                    $uibModalInstance.close("1");
+                }
+            }).catch(function (data) {
+            });
+        } else {//新增操作
+            var SNList = [{ name: "CompleteRpt", col: "DocNo", parm: "ListNo", charName: null }]
+            en.SNColumns = JSON.stringify(SNList);
+            en.ListNo = "";
+            vm.promise = AjaxService.ExecPlan("CompleteRpt", "Add", en).then(function (data) {
+                if (data.data[0].MsgType == "0") {
+                    toastr.error(data.data[0].Msg);
+                } else {
+                    var returnData = "1";
+                    toastr.success(data.data[0].Msg);
+                    $uibModalInstance.close("1");
+                }
+            }).catch(function (data) {
+            });
+        }
+    }
+
+    //取消
+    function Cancel() {
+        $uibModalInstance.close("1");
+    }
+
+    //选择料号
+    function SelectProduct() {
+        var resolve = {
+            ItemData: function () {
+                return {};
+            }
+        }
+        Dialog.open("MesMaterialSer", resolve).then(function (data) {
+            if (data.Id) {
+                vm.Item.MaterialID = data.Id;
+                vm.Item.MaterialCode = data.MaterialCode;
+                vm.Item.MaterialName = data.MaterialName;
+            }
+        }).catch(function (reason) {
+
+        });
+    }
+    //选择工单
+    function SelectWorkOrder() {
+        var resolve = {
+            ItemData: function () {
+                return {};
+            }
+        }
+        Dialog.open("WorkOrderSer", resolve).then(function (data) {
+            if (data.ID) {
+                vm.Item.WorkOrderID = data.ID;
+                vm.Item.WorkOrder = data.WorkOrder;
+                vm.Item.MaterialID = data.MaterialID;
+                vm.Item.MaterialCode = data.MaterialCode;
+                vm.Item.MaterialName = data.MaterialName;
+            }
+        }).catch(function (reason) {
+
+        });
+    }
+
+    //获取当前日期
+    function GetCurrentDate() {
+        var date = new Date();
+        var year = date.getFullYear();
+        var month = date.getMonth() + 1;
+        var day = date.getDate();
+        var h = date.getHours();
+        var m = date.getMinutes();
+        var s = date.getSeconds();
+        var rq = year + "-" + month + "-" + day + " " + h + ":" + m + ":" + s;
+        return rq;
+    }
+
+}
+])
---
+++ MXQH/UIFun/MESFun/MO/WorkOrderSer.js
@@ -0,0 +1,46 @@
+﻿'use strict';
+angular.module('app')
+.controller('WorkOrderSerCtrl', ['$rootScope', '$scope', 'ItemData', '$http', '$uibModalInstance', 'AjaxService', 'toastr', '$window',
+function ($rootScope, $scope, ItemData, $http, $uibModalInstance, AjaxService, toastr, $window) {
+
+    var vm = this;
+    vm.page = { index: 1, size: 8 };
+    vm.Ser = {};
+
+    vm.PageChange = PageChange;
+    vm.Search = Search;
+
+    vm.Cancel = Cancel;
+    PageChange();
+    function Search() {
+        vm.page.index = 1;
+        PageChange();
+    }
+
+    function PageChange() {
+        vm.promise = AjaxService.GetPlansPage("MesPlanDetail", GetContition(), vm.page.index, vm.page.size).then(function (data) {
+            vm.List = data.List;
+            vm.page.total = data.Count;
+        });
+
+    }
+    function GetContition() {
+        var list = [];
+        // list.push({ name: "State", value: '1' });
+        if (vm.Ser.MaterialCode) {
+            list.push({ name: "MaterialCode", value: '%' + vm.Ser.MaterialCode + '%' });
+        }
+        if (vm.Ser.MaterialName) {
+            list.push({ name: "MaterialName", value: '%' + vm.Ser.MaterialName + '%' });
+        }
+        if (vm.Ser.WorkOrder) {
+            list.push({ name: "WorkOrder", value: '%' + vm.Ser.WorkOrder + '%' });
+        }
+        return list;
+    }
+
+    function Cancel(item) {
+        $uibModalInstance.close(item);
+    }
+
+}]);
