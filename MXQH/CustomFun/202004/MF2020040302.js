0a0db469e0aaa506c09619cf3eec46294a2720ad
6009000134 <491675469@qq.com>
2020-04-24 17:11:28 +0800

    mes 泉州销售管里bug修改

--- MXQH/CustomFun/202004/MF2020040302.js
+++ MXQH/CustomFun/202004/MF2020040302.js
@@ -16,6 +16,7 @@ function ($rootScope, $scope, $http, AjaxService, toastr, $window) {
     vm.PageChange = PageChange;
     vm.Search = Search;
     vm.ExportExcel = ExportExcel;
+    vm.IsCodeExist = IsCodeExist;
 
     function Search() {
         vm.page.index = 1;
@@ -34,6 +35,21 @@ function ($rootScope, $scope, $http, AjaxService, toastr, $window) {
             vm.IsInsert = false;
         });
     }
+    //编码是否重复
+    function IsCodeExist(isEdit) {
+        console.log(isEdit);
+        if (isEdit) {//Edit
+            if (vm.NowItem.Code!=vm.EditItem.Code) {
+                vm.promise = AjaxService.GetPlan("QZSOAgent", { name: "Code", value: vm.EditItem.Code }).then(function (data) {
+                    vm.NowItem.ItemForm.item_Code.$setValidity('unique', !data.Code);
+                });
+            }            
+        } else {//Add
+            vm.promise = AjaxService.GetPlan("QZSOAgent", { name: "Code", value: vm.NewItem.Code }).then(function (data) {
+                vm.InsertForm.Code.$setValidity('unique', !data.Code);
+            });
+        }
+    }
 
     function Edit(item) {
         for (var i = 0, len = vm.List.length; i < len; i++) {
@@ -47,10 +63,17 @@ function ($rootScope, $scope, $http, AjaxService, toastr, $window) {
     function Delete(item) {
         var en = angular.copy(item);
         en.ItemForm = undefined;
-        vm.promise = AjaxService.PlanDelete("QZSOAgent", en).then(function (data) {
-            PageChange();
-            toastr.success('删除成功');
+        AjaxService.GetPlansTop("QZSO", { name: "SOAgentID", value: item.ID }).then(function (data) {
+            if (data.length == 0) {
+                vm.promise = AjaxService.PlanDelete("QZSOAgent", en).then(function (data) {
+                    PageChange();
+                    toastr.success('删除成功');
+                });
+            } else {
+                toastr.error("存在该客户销售单，不能删除！");
+            }
         });
+      
     }
 
     function SaveEdit(index) {
