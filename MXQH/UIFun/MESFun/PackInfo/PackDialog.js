8482c05d8d08e7b0bd69b5cde5cacc937aa6a237
6009000134 <491675469@qq.com>
2020-10-29 11:02:57 +0800

    mes 1、工单同步时完工报告类型同步 2、包装登记增加“从头开始计数” 3、完工报告增加U9开工/返工列表

--- MXQH/UIFun/MESFun/PackInfo/PackDialog.js
+++ MXQH/UIFun/MESFun/PackInfo/PackDialog.js
@@ -6,17 +6,18 @@ function ($rootScope, $scope, ItemData, $uibModalInstance, Dialog, toastr, AjaxS
     vm.Item = ItemData;
     vm.Save = Save;
     vm.Cancel = Cancel;
-    vm.Item.PackListNo = vm.Item.PackListNo == undefined ? GetListNo() : vm.Item.PackListNo;
-    console.log(1,vm.Item);
+    vm.Item.PackListNo = vm.Item.PackListNo == undefined ? GenerateListNo() : vm.Item.PackListNo;
+    vm.IsStartAt0 = false;
+    vm.GetListNo = GetListNo;
     GetPlaces();
     if (vm.Item.MaterialID) {//新增操作才会有
         vm.Item.CountryCode = 'CN';//默认CN
         vm.promise = AjaxService.GetPlan("MesMXMaterial", { name: "Id", value: vm.Item.MaterialID }).then(function (data) {
             vm.Item.PerBoxQuantity = data.PerBoxCount;
-            vm.Item.PerColorBoxQty = data.ColorBoxCount;            
+            vm.Item.PerColorBoxQty = data.ColorBoxCount;
         });
         //Tanapa会有多条，多条时，以出货地为条件
-        vm.promise = AjaxService.ExecPlan("MESbaTanapa", "Get", {MaterialID:vm.Item.MaterialID,MoID:vm.Item.MoID}).then(function (data) {
+        vm.promise = AjaxService.ExecPlan("MESbaTanapa", "Get", { MaterialID: vm.Item.MaterialID, MoID: vm.Item.MoID }).then(function (data) {
             vm.Item.Ean = data.data[0].Ean;
             vm.Item.Model = data.data[0].Model;
             vm.Item.RadioKit = data.data[0].RadioKit;
@@ -25,18 +26,27 @@ function ($rootScope, $scope, ItemData, $uibModalInstance, Dialog, toastr, AjaxS
         });
     }
     //
-    if (vm.Item.MoID) {
-        vm.promise = AjaxService.ExecPlan("MESPackageMain", "GetPackListNo", { MoID: vm.Item.MoID }).then(function (data) {
-            if (!vm.Item.TransID) {
-                vm.Item.TransID = data.data[0].CustomerOrder;
-            }
-            //创建包装标签号
-            if (data.data[0].PackListNo == '') {
-                GetListNo();
-            } else {
-                vm.Item.PackListNo = data.data[0].PackListNo;
-            }
-        });
+    GetListNo();
+    //获取包装标签号
+    function GetListNo() {
+        if (vm.Item.MoID) {
+            vm.promise = AjaxService.ExecPlan("MESPackageMain", "GetPackListNo", { MoID: vm.Item.MoID }).then(function (data) {
+                if (!vm.Item.TransID) {
+                    vm.Item.TransID = data.data[0].CustomerOrder;
+                }
+                //创建包装标签号
+                if (vm.IsStartAt0) {
+                    GenerateListNo();
+                }
+                else {
+                    if (data.data[0].PackListNo == '') {
+                        GenerateListNo();
+                    } else {
+                        vm.Item.PackListNo = data.data[0].PackListNo;
+                    }
+                }
+            });
+        }
     }
     //保存
     function Save() {
@@ -51,7 +61,7 @@ function ($rootScope, $scope, ItemData, $uibModalInstance, Dialog, toastr, AjaxS
         vm.Item.PKGWT = vm.Item.PKGWT == undefined ? '' : vm.Item.PKGWT;
         vm.Item.RadioKit = vm.Item.RadioKit == undefined ? '' : vm.Item.RadioKit;
         if (vm.Item.ID) {
-        vm.Item.Order = undefined;
+            vm.Item.Order = undefined;
         }
         li.push(vm.Item);
         en.Entity = JSON.stringify(li);
@@ -70,6 +80,7 @@ function ($rootScope, $scope, ItemData, $uibModalInstance, Dialog, toastr, AjaxS
             var SNList = [{ name: "PackInfo", col: "Label", parm: "PackListNo", charName: null }]
             en.SNColumns = JSON.stringify(SNList);
             en.PackListNo = "";
+            en.IsStartAt0 = vm.IsStartAt0;//是否从0开始计算箱号
             vm.promise = AjaxService.ExecPlan("MESPackageMain", "Add", en).then(function (data) {
                 if (data.data[0].MsgType == '1') {
                     toastr.success(data.data[0].Msg);
@@ -82,7 +93,7 @@ function ($rootScope, $scope, ItemData, $uibModalInstance, Dialog, toastr, AjaxS
     }
 
     //自动生成计划序号
-    function GetListNo() {
+    function GenerateListNo() {
         var en = { TbName: "PackInfo", ClName: "Label", CharName: null };
         AjaxService.ExecPlan("SerialNumberSet", "preview", en).then(function (data) {
             vm.Item.PackListNo = data.data[0].SN;
