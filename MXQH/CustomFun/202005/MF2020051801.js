01d015600a9b682b81d4c921815dfe0dcffdfc5e
6009000134 <491675469@qq.com>
2021-06-02 17:30:27 +0800

    1、包装工单报表修改 2、工单软件控制录入

---
+++ MXQH/CustomFun/202005/MF2020051801.js
@@ -0,0 +1,32 @@
+﻿'use strict';
+
+angular.module('AppSet')
+.controller('RDStoreCtrl', ['$scope', '$http', 'AjaxService', 'toastr', '$window',
+function ($scope, $http, AjaxService, toastr, $window) {
+
+    var vm = this;
+    vm.page = { pageIndex: 1, pageSize: 10 };
+
+    vm.PageChange = PageChange;
+    vm.Search = Search;
+    vm.ExportExcel = ExportExcel;
+
+    function Search() {
+        vm.page.pageIndex = 1;
+        PageChange();
+    }
+
+    function PageChange() {
+        vm.promise = AjaxService.ExecPlan("RDReceivement", "Report", vm.page).then(function (data) {            
+            vm.List = data.data;
+            vm.page.total = data.data1[0].Count;
+        });
+    }
+
+    function ExportExcel() {
+        vm.promise = AjaxService.GetPlanExcel("RDReceivement", "Report", vm.Ser).then(function (data) {
+            $window.location.href = data.File;
+        });
+    }
+
+}]);
---
+++ MXQH/CustomFun/2021/Dialog/DiaLog2021060201.js
@@ -0,0 +1,62 @@
+﻿'use strict';
+
+angular.module('AppSet')
+.controller('mosoftcontrol', ['$scope', 'ItemData', '$uibModalInstance', 'AjaxService', 'toastr', '$window',
+function ($scope, ItemData, $uibModalInstance, AjaxService, toastr, $window) {
+
+    var vm = this;
+    vm.page = { index: 1, size: 10 };
+    vm.Ser = {};
+
+    vm.PageChange = PageChange;
+    vm.Search = Search;
+    vm.OK = OK;
+    vm.Cancel = Cancel;
+    vm.Select = Select;
+    vm.OKClose = OKClose;
+
+    Search();
+    function Search() {
+        vm.page.index = 1;
+        PageChange();
+    }
+
+    function PageChange() {
+        vm.promise = AjaxService.GetPlansPage("MOAndWPO", GetContition(), vm.page.index, vm.page.size).then(function (data) {
+            vm.List = data.List;
+            vm.page.total = data.Count;
+        });
+
+    }
+    function GetContition() {
+        var list = [];
+        if (vm.Ser.aDocNo) {
+            list.push({ name: "DocNo", value: vm.Ser.aDocNo, tableAs:"a" });
+        }
+        if (vm.Ser.aWPO) {
+            list.push({ name: "WPO", value: vm.Ser.aWPO, tableAs:"a" });
+        }
+        if (vm.Ser.aCode) {
+            list.push({ name: "Code", value: vm.Ser.aCode, tableAs:"a" });
+        }
+        if (ItemData.SerList && ItemData.SerList.length > 0) {
+            for (var i = 0, len = ItemData.SerList.length; i < len; i++) {
+                list.push(ItemData.SerList[i]);
+            }
+        }
+        return list;
+    }
+    function OK(item) {
+        $uibModalInstance.close(item);
+    }
+    function Select(item) {
+        vm.SerItem = angular.copy(item);
+    }
+    function OKClose() {
+        $uibModalInstance.close(vm.SerItem);
+    }
+    function Cancel() {
+        $uibModalInstance.dismiss('cancel');
+    }
+
+}]);
