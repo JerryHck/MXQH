8bdc794fead07d8aa5038691e9dd0de980b6495c
6009000134 <491675469@qq.com>
2021-07-21 12:08:46 +0800

    1、排产报表优化

--- MXQH/UIFun/MESFun/Report/ScheduleReport.js
+++ MXQH/UIFun/MESFun/Report/ScheduleReport.js
@@ -23,15 +23,20 @@ function ($rootScope, $scope, $http, AjaxService, toastr, $window, Form) {
 
     function DataBind() {
         var en = {};
-        var enFrom = { WorkOrder: vm.Ser.WorkOrder ,MRPCategory:vm.Ser.MRPCategory};
-        en.fromConn = 'U9con';
-        en.fromProc = 'dbo.sp_mes_GetMO';
-        en.fromJson = JSON.stringify(enFrom);
-        en.toConn = 'MEScon';
-        en.toProc = 'dbo.sp_GetScheduleReport';
-        en.toJson = JSON.stringify({ WorkOrder: vm.Ser.WorkOrder, pageSize: vm.Ser.pageSize, pageIndex: vm.Ser.pageIndex, LineID: vm.Ser.LineID, SD: vm.Ser.SD, ED: vm.Ser.ED });
+        en.fromConn = 'MEScon';
+        en.fromProc = 'dbo.sp_GetScheduleReport';
+        en.fromJson = JSON.stringify({ WorkOrder: vm.Ser.WorkOrder, pageSize: vm.Ser.pageSize, pageIndex: vm.Ser.pageIndex, LineID: vm.Ser.LineID, SD: vm.Ser.SD, ED: vm.Ser.ED });
+
+        en.toConn = 'U9con';
+        en.toProc = 'dbo.sp_mes_GetPlanData';
+        en.toJson = JSON.stringify({ WorkOrder: vm.Ser.WorkOrder, MRPCategory: vm.Ser.MRPCategory });
+
+        //en.toConn = 'MEScon';
+        //en.toProc = 'dbo.sp_GetScheduleReport';
+        //en.toJson = JSON.stringify({ WorkOrder: vm.Ser.WorkOrder, pageSize: vm.Ser.pageSize, pageIndex: vm.Ser.pageIndex, LineID: vm.Ser.LineID, SD: vm.Ser.SD, ED: vm.Ser.ED });
         vm.promise = AjaxService.BasicCustom("ExecProcCross", en).then(function (data) {
             vm.List = data.data;
+            console.log(data);
             vm.Ser.total = data.data1[0].Count;
         });
         
@@ -42,19 +47,26 @@ function ($rootScope, $scope, $http, AjaxService, toastr, $window, Form) {
         var list = [];
         var en = {};
         var en2 = {};
-        en.Conn = 'U9con';
-        en.Proc = 'dbo.sp_mes_GetMO';
-        en.Json = JSON.stringify({ WorkOrder: vm.Ser.WorkOrder, MRPCategory: vm.Ser.MRPCategory });
+
+        en.shortName = 'Schedule';
+        en.planName = 'MesPlanDetail';
+        en.Json = JSON.stringify({ WorkOrder: vm.Ser.WorkOrder, pageSize: vm.Ser.pageSize, pageIndex: vm.Ser.pageIndex, LineID: vm.Ser.LineID, SD: vm.Ser.SD, ED: vm.Ser.ED });
         en.IsTrans = false;
 
-        en2.shortName = 'Schedule';
-        en2.planName = 'MesPlanDetail';
-        en2.Json = JSON.stringify({ WorkOrder: vm.Ser.WorkOrder, pageSize: vm.Ser.pageSize, pageIndex: vm.Ser.pageIndex, LineID: vm.Ser.LineID, SD: vm.Ser.SD, ED: vm.Ser.ED });
+        en2.Conn = 'U9con';
+        en2.Proc = 'dbo.sp_mes_GetPlanData';
+        en2.Json = JSON.stringify({ WorkOrder: vm.Ser.WorkOrder, MRPCategory: vm.Ser.MRPCategory });
         en2.IsTrans = false;
+
+        
+        //en2.shortName = 'Schedule';
+        //en2.planName = 'MesPlanDetail';
+        //en2.Json = JSON.stringify({ WorkOrder: vm.Ser.WorkOrder, pageSize: vm.Ser.pageSize, pageIndex: vm.Ser.pageIndex, LineID: vm.Ser.LineID, SD: vm.Ser.SD, ED: vm.Ser.ED });
+        //en2.IsTrans = false;
+
         list.push(en);
         list.push(en2);
         enx.strJson = JSON.stringify(list);
-        console.log(enx);
         vm.promise = AjaxService.BasicCustom("ExecProcMultiExcel", enx).then(function (data) {
             console.log(data);
         });
