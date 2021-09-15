ce47778bb3d366f9e30d82cd114539cd236fd4a9
6009000134 <491675469@qq.com>
2021-09-15 09:33:34 +0800

    模具寿命报表

---
+++ MXQH/CustomFun/2021/Fun/MF2021090801.js
@@ -0,0 +1,46 @@
+﻿'use strict';
+
+angular.module('AppSet')
+.controller('MouldLifCtrl', ['$scope', 'Dialog', 'AjaxService', 'toastr', '$window',
+function ($scope, Dialog, AjaxService, toastr, $window) {
+
+    var vm = this;
+    vm.page = { index: 1, size: 12 };
+    vm.Ser = {};
+
+    vm.PageChange = PageChange;
+    vm.Search = Search;
+    vm.ExportExcel = ExportExcel;
+
+    function Search() {
+        vm.page.index = 1;
+        PageChange();
+    }
+
+    function PageChange() {
+        vm.promise = AjaxService.GetPlansPage("MouldLifeRpt", GetContition(), vm.page.index, vm.page.size).then(function (data) {
+            vm.List = data.List;
+            vm.page.total = data.Count;
+        });
+
+    }
+    function ExportExcel() {
+        vm.promise = AjaxService.GetPlanOwnExcel("MouldLifeRpt", GetContition()).then(function (data) {
+            $window.location.href = data.File;
+        });
+    }
+    function GetContition() {
+        var list = [];
+        if (vm.Ser.aCode) {
+            list.push({ name: "Code", value: vm.Ser.aCode, tableAs:"a" });
+        }
+        if (vm.Ser.aName) {
+            list.push({ name: "Name", value: vm.Ser.aName, tableAs:"a" });
+        }
+        if (vm.Ser.aHolder) {
+            list.push({ name: "Holder", value: vm.Ser.aHolder, tableAs:"a" });
+        }
+        return list;
+    }
+
+}]);
